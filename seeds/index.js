163d57876acefeca7c562fea31fcbcdd6edc9fdf
jasmeeeetsingh17 <jasmeetsingh1710@gmail.com>
2025-08-28 11:34:53 +0530

    adding styles

--- seeds/index.js
+++ seeds/index.js
@@ -19,7 +19,10 @@ const seedDB = async () => {
     for (let i = 0; i < 50; i++) {
         const camp = new Campground({
             title: `${random(descriptors)} ${random(places)}`,
-            location: `${random(cities).city}, ${random(cities).state}`
+            location: `${random(cities).city}, ${random(cities).state}`,
+            image: `https://picsum.photos/400?random=${Math.random()}`,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium rem repudiandae eum blanditiis nulla impedit, beatae saepe at deserunt doloribus quod natus ex voluptate numquam magnam ducimus neque omnis eligendi.',
+            price: `$ ${Math.floor(Math.random() * 50)} `
         })
         await camp.save();
     }
